671059a940e58a8b047f743a9d59ad68051a80a3
joséphine <cotofrancejapon@gmail.com>
2019-04-13 10:31:27 +0000

    user registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root to: 'toppages#index'
   
+  get 'signup', to: 'users#new'
+  resources :users, only: [:show, :new, :create]
+
 end
