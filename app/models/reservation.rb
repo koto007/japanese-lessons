c41e663b5f5f515e34f2e117e22c2d5a730ca399
joséphine <cotofrancejapon@gmail.com>
2019-05-04 16:32:04 +0000

    reservation calendar

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -16,17 +16,17 @@ class Reservation < ApplicationRecord
   
   validate :reservation_quota, :on => :create 
 
-  private 
-    def reservation_quota
-      user.reservations.map(&:reserved_at)
-      count = user.reservations.map(&:reserved_at).count
-      dates = Date.parse(user.reservations.map(&:reserved_at)[0..10]).to_s
-      if user.reservations.count > 1 && dates == Date.today + 1.day
-         errors.add(:base, "Vous avez déjà réserver un cours à ce jour-là.")
+#  private 
+ #   def reservation_quota
+  #    user.reservations.map(&:reserved_at)
+   #   count = user.reservations.map(&:reserved_at).count
+    #  dates = Date.parse(user.reservations.map(&:reserved_at)[0..10]).to_s
+     # if user.reservations.count > 1 && dates == Date.today + 1.day
+      #   errors.add(:base, "Vous avez déjà réserver un cours à ce jour-là.")
       #elsif user.reservations.count > 2
        #  errors.add(:base, "Vous pouvez réserver deux cours par semaine.")
-      end
-    end  
+      #end
+    #end  
   
   
 end
